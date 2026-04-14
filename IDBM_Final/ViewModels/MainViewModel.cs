7eeaac0c9527fa96756ae56784500a1503f0732b
Revny <Lsilver205@gmail.com>
2026-04-13 22:21:15 -0300

    Added a few small file just to get them done with now

--- IDBM_Final/ViewModels/MainViewModel.cs
+++ IDBM_Final/ViewModels/MainViewModel.cs
@@ -1,10 +1,45 @@
-﻿using System;
+﻿using IDBM_Final.Comands;
+using IDBM_Final.Services;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
+using System.Windows.Input;
 
 namespace IDBM_Final.ViewModels
 {
-    internal class MainViewModel
+    public class MainViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private object _currentViewModel;
+        private readonly INavigationService _navigationService;
+
+
+
+        public object CurrentViewModel
+        {
+            get => _currentViewModel;
+            set
+            {
+                if (_currentViewModel != value)
+                {
+                    _currentViewModel = value;
+                    OnPropertyChanged(nameof(CurrentViewModel));
+                }
+            }
+        }
+        public MainViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+            CurrentViewModel = new HomeViewModel();
+        }
+
+        public ICommand NavToHomeCommand => new RelayCommand(_ => _navigationService.NavigateTo<HomeViewModel>());
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
